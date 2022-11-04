ae4bce4cb94dcfd9f03c822e0edd2394fd10449e
Eldiyar888 <jashtilekov888@gmail.com>
2022-11-04 16:43:00 +0600

    first commit

---
+++ Print_matrix/script.js
@@ -0,0 +1,20 @@
+const arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+const main = [];
+const side = [];
+
+const n = arr.length;
+
+for(let i = 0; i < n; i++) {
+    console.log(arr[i]);
+}
+
+for(let i = 0; i < n; i++) {
+    main[i] = arr[i][i];
+}
+
+for(let i = 0; i < n; i++) {
+    side[i] = arr[i][n - 1 - i];
+}
+
+console.log("главная диагональ", main);
+console.log("побочная диагональ", side);
